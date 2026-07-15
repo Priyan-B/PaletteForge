ca12a433df4c1f4f006a3209fdc6ca682b279d31
Aishwarya Rajmohan <aishcw6498@gmail.com>
2026-07-15 20:25:49 +0530

    Added 2 tabs for independent work, passport auth placeholder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import IndexPage from "./components/IndexPage.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <IndexPage />
+    <App />
   </StrictMode>
 );
